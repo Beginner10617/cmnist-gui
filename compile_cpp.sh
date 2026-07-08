cdd0f2adc7c4f13afc4cab59b791f83a3335d32a
Beginner10617 <wasihusain23@iitk.ac.in>
2026-07-08 19:39:49 +0530

    Added bash file to compile cpp frontend

---
+++ compile_cpp.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+g++ -c main.cpp -I./SDL/include/ -I./imgui/ -I./imgui/backends/ -o build/main.o
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 extern "C" {
 #include "cmnist/neuron_utils.h"
 }
- int main(){
-   Value *v = createNewValue(1.2f, "hello");
-   return 0;
- }
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl2.h"
+int main() {
+  Value *v = createNewValue(1.2f, "hello");
+  return 0;
+}
