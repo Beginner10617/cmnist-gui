ff4863199d328609b30afff9afff91e46b8a56ec
Beginner10617 <wasihusain23@iitk.ac.in>
2026-07-11 18:44:06 +0530

    Added docking, removed multi-viewports

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ extern "C" {
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl2.h"
+#include "imgui_internal.h"
 #include <stdio.h>
 #include <vector>
 
@@ -33,11 +34,12 @@ int main() {
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
   float content_scale = ImGui_ImplSDL2_GetContentScaleForDisplay(0);
-  SDL_WindowFlags flags =
-      (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
-  int factor = 50;
-  SDL_Window *window =
-      SDL_CreateWindow("CMNIST", 50, 50, 16 * factor, 9 * factor, flags);
+  SDL_WindowFlags SDL_WINDOW_FLAGS =
+      (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI |
+                        SDL_WINDOW_RESIZABLE);
+  int factor = 60;
+  SDL_Window *window = SDL_CreateWindow("CMNIST", 50, 50, 16 * factor,
+                                        9 * factor, SDL_WINDOW_FLAGS);
   if (window == nullptr) {
     printf("Error while calling SDL_CreateWindow: %s\n", SDL_GetError());
     exit(EXIT_FAILURE);
@@ -59,11 +61,12 @@ int main() {
   // io config
   ImGuiIO &io = ImGui::GetIO();
   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+  // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
   // theme/style
   ImGui::StyleColorsDark(); // Dark mode
   ImGuiStyle &style = ImGui::GetStyle();
+  style.WindowBorderSize = 0.0f;
   style.ScaleAllSizes(content_scale);
   style.FontScaleDpi = content_scale;
 
@@ -90,7 +93,9 @@ int main() {
   bool drawing = false;
   ImVec2 current_posn;
   unsigned int current_stroke_index;
-
+  float BRUSH_WIDTH = 2.0f;
+  ImVec2 cursor;
+  bool first_frame = true;
   // main-loop
   bool running = true;
   while (running) {
@@ -115,12 +120,55 @@ int main() {
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 
-    // Add widgets and windows
-    ImGui::SetNextWindowPos(ImVec2(60, 100), ImGuiCond_Once);
-    ImGui::Begin("Canvas");
+    // docking
+    ImGuiID dockspace_id = ImGui::GetID("My Dockspace");
+    ImGuiViewport *viewport = ImGui::GetMainViewport();
+    if (ImGui::DockBuilderGetNode(dockspace_id) == nullptr) {
+      ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+      ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+      ImGuiID dock_id_right = 0;
+      ImGuiID dock_id_main = dockspace_id;
+      ImGui::DockBuilderSplitNode(dock_id_main, ImGuiDir_Left, 0.50f,
+                                  &dock_id_main, &dock_id_right);
+      ImGui::DockBuilderDockWindow("Canvas", dock_id_main);
+      ImGui::DockBuilderDockWindow("Prediction", dock_id_right);
+      ImGui::DockBuilderFinish(dockspace_id);
+    }
+    ImGui::DockSpaceOverViewport(dockspace_id, viewport,
+                                 ImGuiDockNodeFlags_PassthruCentralNode);
+    // ImGui::Begin("Toolbar", nullptr);
+    // ImGui::End();
+
+    ImGui::Begin("Prediction", nullptr);
+    if (ImGui::Button("File")) {
+      // load model from text file
+    }
+    ImGui::SameLine(750);
+    if (ImGui::Button("Clear")) {
+      strokes.clear();
+    }
+    ImGui::End();
+
+    // Canvas
+    ImGui::Begin("Canvas", nullptr);
+    if (ImGui::Button("File")) {
+      // load model from text file
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Help")) {
+      // load help etc
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Clear")) {
+      strokes.clear();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Predict")) {
+      // call backend to display prediction
+    }
     ImDrawList *draw_list = ImGui::GetWindowDrawList();
-    ImVec2 cursor = ImGui::GetCursorScreenPos();
-    ImVec2 canvas_dimension(7 * factor, 8 * factor * 10 / 12);
+    cursor = ImGui::GetCursorScreenPos();
+    ImVec2 canvas_dimension(15 * factor / 2, 9 * factor * 10 / 12);
     draw_list->AddRectFilled(cursor, cursor + canvas_dimension, IM_COL32_WHITE);
     ImGui::InvisibleButton("Canvas", canvas_dimension);
     bool hovered = ImGui::IsItemHovered();
@@ -138,12 +186,11 @@ int main() {
 
     if (drawing)
       strokes[current_stroke_index].points.push_back(current_posn);
-    for (Stroke stroke : strokes) {
+    for (const Stroke &stroke : strokes) {
       for (int i = 1; i < stroke.points.size(); i++)
         draw_list->AddLine(stroke.points[i - 1], stroke.points[i],
-                           IM_COL32_BLACK, 2.0f);
+                           IM_COL32_BLACK, BRUSH_WIDTH);
     }
-
     ImGui::End();
 
     // rendering
@@ -156,13 +203,6 @@ int main() {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     // rendering other non-main viewports (required for multi-viewports)
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-      SDL_Window *og_window = SDL_GL_GetCurrentWindow();
-      SDL_GLContext og_context = SDL_GL_GetCurrentContext();
-      ImGui::UpdatePlatformWindows();
-      ImGui::RenderPlatformWindowsDefault();
-      SDL_GL_MakeCurrent(og_window, og_context);
-    }
 
     // swap window call for sdl
     SDL_GL_SwapWindow(window);
